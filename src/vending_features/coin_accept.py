a40f5a256d757d097d59347acef04c58f26b71e6
Nnamdi Kalu <nnamdi.kalu@gmail.com>
2020-06-24 22:47:58 -0400

    added functionality and tests for accepting coins

---
+++ src/vending_features/coin_accept.py
@@ -0,0 +1,44 @@
+class CoinAcceptor(object):
+    def __init__(self):
+        # keep track of value of coins inserted
+        self.coin_value = 0
+        # number of coins inserted not in coin return
+        self.number_of_coins = 0
+        super().__init__()
+
+    def accept_coins(self, coin_inserted: str):
+        # this functions will determine the value to assign
+        # the coin based on the length first (size)
+        # the string is then lowered and sorted and compared (weight)
+        # if the coin is invalid, it is sent to the coin_return function
+        if len(coin_inserted) == 4:
+            if self.weigh_coin(coin_inserted) == 'deim':
+                self.coin_value += 10
+                self.number_of_coins += 1
+        elif len(coin_inserted) == 6:
+            if self.weigh_coin(coin_inserted) == 'ceikln':
+                self.coin_value += 5
+                self.number_of_coins += 1
+            else:
+                self.coin_return(coin_inserted)
+        elif len(coin_inserted) == 7:
+            if self.weigh_coin(coin_inserted) == 'aeqrrtu':
+                self.coin_value += 25
+                self.number_of_coins += 1
+            else:
+                self.coin_return(coin_inserted)
+        else:
+            self.coin_return(coin_inserted)
+
+    def coin_display(self) -> str:
+        if self.number_of_coins:
+            return f'{self.coin_value}'
+        return 'Insert Coin'
+
+    def weigh_coin(self, coin_to_weigh:str) -> str:
+        # returns the "weight" of the coin
+        return ''.join(sorted(coin_to_weigh.lower()))
+
+    def coin_return(self, coin_to_return: str):
+        # ust prints that a coin has been sent to Coin Return
+        return f'{coin_to_return} sent to Coin Return'
---
+++ tests/test_coin_accept.py
@@ -0,0 +1,26 @@
+from src.vending_features.coin_accept import CoinAcceptor
+import unittest
+
+class CoinAcceptorTest(unittest.TestCase):
+    def setUp(self):
+        self.coin_accept = CoinAcceptor()
+
+    def test_weigh_coin(self):
+        self.assertEqual(self.coin_accept.weigh_coin('dime'), 'deim')
+        self.assertEqual(self.coin_accept.weigh_coin('penny'), 'ennpy')
+        self.assertEqual(self.coin_accept.weigh_coin('nickel'), 'ceikln')
+        self.assertEqual(self.coin_accept.weigh_coin('quarter'), 'aeqrrtu')
+
+    def test_coin_display(self):
+        self.assertEqual(self.coin_accept.coin_display(), 'Insert Coin')
+        self.coin_accept.accept_coins('nickel')
+        self.assertEqual(self.coin_accept.coin_display(), '5')
+
+    def test_accept_coin(self):
+        self.coin_accept.accept_coins('quarter')
+        self.assertEqual(self.coin_accept.coin_value, 25)
+        self.coin_accept.accept_coins('penny')
+        self.assertEqual(self.coin_accept.coin_value, 25)
+
+if __name__ == '__main__':
+    unittest.main()
